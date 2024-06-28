9543dfc52934a3618ea5597fb3e4c13ad91da87f
Przemek <prz.ziaja@gmail.com>
2024-06-28 20:00:10 +0200

    update s3 in secret

--- src/cifar/constants/secret.py
+++ src/cifar/constants/secret.py
@@ -2,5 +2,5 @@ s3_secrets = {
     "use_ssl": False,
     "key": "...",
     "secret": "...",
-    "client_kwargs": {"endpoint_url": "http://localhost:9000"},
+    "client_kwargs": {"endpoint_url": "http://s3:9000"},
 }
