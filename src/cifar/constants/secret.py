321f5bf50f10e7fe742b665f44e75d88b27adeb9
Przemek <prz.ziaja@gmail.com>
2024-06-30 09:32:38 +0200

    update s3 connection

--- src/cifar/constants/secret.py
+++ src/cifar/constants/secret.py
@@ -4,3 +4,9 @@ s3_secrets = {
     "secret": "...",
     "client_kwargs": {"endpoint_url": "http://s3:9000"},
 }
+
+s3_secrets_pa = {
+    "access_key": "...",
+    "secret_key": "...",
+    "endpoint_override": "http://s3:9000"
+}
--- src/cifar/io/utils.py
+++ src/cifar/io/utils.py
@@ -1,7 +1,12 @@
 import s3fs
+from pyarrow import fs
 
-from cifar.constants.secret import s3_secrets
+from cifar.constants.secret import s3_secrets_pa, s3_secrets
 
+
+def get_s3_fs_pa():
+    fs_pa = fs.S3FileSystem(**s3_secrets_pa)
+    return fs_pa
 
 def get_s3_fs():
     fs = s3fs.S3FileSystem(anon=False, **s3_secrets)
